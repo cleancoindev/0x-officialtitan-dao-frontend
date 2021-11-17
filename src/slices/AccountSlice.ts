905373398609ed0b64ca3d9ce58e4a88ce826ec0
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2021-11-17 09:30:34 -1000

    Initial commit

---
+++ src/slices/AccountSlice.ts
@@ -0,0 +1,235 @@
+import { ethers } from "ethers";
+import { addresses } from "../constants";
+import { abi as ierc20Abi } from "../abi/IERC20.json";
+import { abi as sOHM } from "../abi/sOHM.json";
+import { abi as sOHMv2 } from "../abi/sOhmv2.json";
+import { abi as fuseProxy } from "../abi/FuseProxy.json";
+
+import { setAll } from "../helpers";
+
+import { createAsyncThunk, createSelector, createSlice } from "@reduxjs/toolkit";
+import { JsonRpcProvider, StaticJsonRpcProvider } from "@ethersproject/providers";
+import { Bond, NetworkID } from "src/lib/Bond"; // TODO: this type definition needs to move out of BOND.
+
+interface IGetBalances {
+  address: string;
+  networkID: NetworkID;
+  provider: StaticJsonRpcProvider | JsonRpcProvider;
+}
+
+export const getBalances = createAsyncThunk(
+  "account/getBalances",
+  async ({ address, networkID, provider }: IGetBalances) => {
+    const ohmContract = new ethers.Contract(addresses[networkID].OHM_ADDRESS as string, ierc20Abi, provider);
+    const ohmBalance = await ohmContract.balanceOf(address);
+    const sohmContract = new ethers.Contract(addresses[networkID].SOHM_ADDRESS as string, ierc20Abi, provider);
+    const sohmBalance = await sohmContract.balanceOf(address);
+
+    return {
+      balances: {
+        ohm: ethers.utils.formatUnits(ohmBalance, "gwei"),
+        sohm: ethers.utils.formatUnits(sohmBalance, "gwei"),
+      },
+    };
+  },
+);
+
+interface ILoadAccountDetails {
+  address: string;
+  networkID: NetworkID;
+  provider: StaticJsonRpcProvider | JsonRpcProvider;
+}
+
+export const loadAccountDetails = createAsyncThunk(
+  "account/loadAccountDetails",
+  async ({ networkID, provider, address }: ILoadAccountDetails) => {
+    let ohmBalance = 0;
+    let sohmBalance = 0;
+    let fsohmBalance = 0;
+    let oldsohmBalance = 0;
+    let stakeAllowance = 0;
+    let unstakeAllowance = 0;
+    let lpStaked = 0;
+    let pendingRewards = 0;
+    let lpBondAllowance = 0;
+    let daiBondAllowance = 0;
+    let aOHMAbleToClaim = 0;
+    let migrateContract;
+    let unstakeAllowanceSohm;
+
+    const daiContract = new ethers.Contract(addresses[networkID].DAI_ADDRESS as string, ierc20Abi, provider);
+    const daiBalance = await daiContract.balanceOf(address);
+
+    if (addresses[networkID].OHM_ADDRESS) {
+      const ohmContract = new ethers.Contract(addresses[networkID].OHM_ADDRESS as string, ierc20Abi, provider);
+      ohmBalance = await ohmContract.balanceOf(address);
+      stakeAllowance = await ohmContract.allowance(address, addresses[networkID].STAKING_HELPER_ADDRESS);
+    }
+
+    if (addresses[networkID].SOHM_ADDRESS) {
+      const sohmContract = await new ethers.Contract(addresses[networkID].SOHM_ADDRESS as string, sOHMv2, provider);
+      sohmBalance = await sohmContract.balanceOf(address);
+      unstakeAllowance = await sohmContract.allowance(address, addresses[networkID].STAKING_ADDRESS);
+    }
+
+    for (const fuseAddressKey of ["FUSE_6_SOHM", "FUSE_18_SOHM"]) {
+      if (addresses[networkID][fuseAddressKey]) {
+        const fsohmContract = await new ethers.Contract(
+          addresses[networkID][fuseAddressKey] as string,
+          fuseProxy,
+          provider,
+        );
+        fsohmContract.signer;
+        const exchangeRate = ethers.utils.formatEther(await fsohmContract.exchangeRateStored());
+        const balance = ethers.utils.formatUnits(await fsohmContract.balanceOf(address), "gwei");
+        fsohmBalance += Number(balance) * Number(exchangeRate);
+      }
+    }
+
+    if (addresses[networkID].OLD_SOHM_ADDRESS) {
+      const oldsohmContract = await new ethers.Contract(
+        addresses[networkID].OLD_SOHM_ADDRESS as string,
+        sOHM,
+        provider,
+      );
+      oldsohmBalance = await oldsohmContract.balanceOf(address);
+
+      const signer = provider.getSigner();
+      unstakeAllowanceSohm = await oldsohmContract.allowance(address, addresses[networkID].OLD_STAKING_ADDRESS);
+    }
+
+    return {
+      balances: {
+        dai: ethers.utils.formatEther(daiBalance),
+        ohm: ethers.utils.formatUnits(ohmBalance, "gwei"),
+        sohm: ethers.utils.formatUnits(sohmBalance, "gwei"),
+        fsohm: fsohmBalance,
+        oldsohm: ethers.utils.formatUnits(oldsohmBalance, "gwei"),
+      },
+      staking: {
+        ohmStake: +stakeAllowance,
+        ohmUnstake: +unstakeAllowance,
+      },
+      migrate: {
+        unstakeAllowance: +unstakeAllowanceSohm,
+      },
+      bonding: {
+        daiAllowance: daiBondAllowance,
+      },
+    };
+  },
+);
+
+interface ICalcUserBondDetails {
+  address: string;
+  bond: Bond;
+  provider: StaticJsonRpcProvider | JsonRpcProvider;
+  networkID: NetworkID;
+}
+export interface IUserBondDetails {
+  allowance: number;
+  interestDue: number;
+  bondMaturationBlock: number;
+  pendingPayout: string; //Payout formatted in gwei.
+}
+export const calculateUserBondDetails = createAsyncThunk(
+  "account/calculateUserBondDetails",
+  async ({ address, bond, networkID, provider }: ICalcUserBondDetails, { dispatch }) => {
+    if (!address) return;
+
+    // dispatch(fetchBondInProgress());
+
+    // Calculate bond details.
+    const bondContract = bond.getContractForBond(networkID, provider);
+    const reserveContract = bond.getContractForReserve(networkID, provider);
+
+    let interestDue, pendingPayout, bondMaturationBlock;
+
+    const bondDetails = await bondContract.bondInfo(address);
+    interestDue = bondDetails.payout / Math.pow(10, 9);
+    bondMaturationBlock = +bondDetails.vesting + +bondDetails.lastBlock;
+    pendingPayout = await bondContract.pendingPayoutFor(address);
+
+    let allowance,
+      balance = 0;
+    allowance = await reserveContract.allowance(address, bond.getAddressForBond(networkID));
+    balance = await reserveContract.balanceOf(address);
+    const balanceVal = ethers.utils.formatEther(balance);
+
+    return {
+      bond: bond.name,
+      displayName: bond.displayName,
+      bondIconSvg: bond.bondIconSvg,
+      isLP: bond.isLP,
+      allowance: Number(allowance),
+      balance: Number(balanceVal),
+      interestDue,
+      bondMaturationBlock,
+      pendingPayout: ethers.utils.formatUnits(pendingPayout, "gwei"),
+    };
+  },
+);
+
+// TODO: update AccountSlice to accurately match the real data.
+interface IAccountSlice {
+  bonds: { [key: string]: IUserBondDetails };
+  [key: string]: any;
+}
+const initialState: IAccountSlice = { bonds: {} };
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    fetchAccountSuccess(state, action) {
+      setAll(state, action.payload);
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(loadAccountDetails.pending, state => {
+        state.loading = true;
+      })
+      .addCase(loadAccountDetails.fulfilled, (state, action) => {
+        setAll(state, action.payload);
+        state.loading = false;
+      })
+      .addCase(loadAccountDetails.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      })
+      .addCase(getBalances.pending, state => {
+        state.loading = true;
+      })
+      .addCase(getBalances.fulfilled, (state, action) => {
+        setAll(state, action.payload);
+        state.loading = false;
+      })
+      .addCase(getBalances.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      })
+      .addCase(calculateUserBondDetails.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(calculateUserBondDetails.fulfilled, (state, action) => {
+        if (!action.payload) return;
+        const bond = action.payload.bond;
+        state.bonds[bond] = action.payload;
+        state.loading = false;
+      })
+      .addCase(calculateUserBondDetails.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      });
+  },
+});
+
+export default accountSlice.reducer;
+
+export const { fetchAccountSuccess } = accountSlice.actions;
+
+// TODO: Update the type of `state` when we have state definitions
+const baseInfo = (state: any) => state.account;
+
+export const getAccountState = createSelector(baseInfo, account => account);
